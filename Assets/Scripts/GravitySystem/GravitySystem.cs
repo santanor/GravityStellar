c90f4b0e620ed756bd7974f2ff1cfa5c8743d130
Jose Santano <jose.santano@microsoft.com>
2018-03-03 15:25:17 +0000

    [+] Spawn dots routine

--- Assets/Scripts/GravitySystem/GravitySystem.cs
+++ Assets/Scripts/GravitySystem/GravitySystem.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace GravitySystem
@@ -19,11 +20,40 @@ namespace GravitySystem
             GravitySources = new List<GravitySource>();
         }
 
-        void AddGravitySource( GravitySource gs )
+        /// <summary>
+        /// Used to add a new gravity source to the list. It'll fire an event to notify everyone
+        /// </summary>
+        /// <param name="gs"></param>
+        public void AddGravitySource( GravitySource gs )
         {
-
+            GravitySources.Add(gs);
+            OnNewGravitySource?.Invoke(gs);
         }
 
+        /// <summary>
+        /// Used to remove the gravity source from the list. It'll also trigger an event to notify everyone
+        /// </summary>
+        /// <param name="gs"></param>
+        public void DestroyGravitySource( GravitySource gs )
+        {
+            GravitySources.Remove(gs);
+            OnDestroyedGravitySource?.Invoke(gs);
+        }
 
+    }
+
+    public static class MakeScriptableObject {
+        [MenuItem("Assets/Create/GravitySystem")]
+        public static void CreateMyAsset()
+        {
+            var asset = ScriptableObject.CreateInstance<GravitySystem>();
+
+            AssetDatabase.CreateAsset(asset, "Assets/Scripts/GravitySystem/GravitySytem.asset");
+            AssetDatabase.SaveAssets();
+
+            EditorUtility.FocusProjectWindow();
+
+            Selection.activeObject = asset;
+        }
     }
 }
--- Assets/Scripts/Models/Star.cs
+++ Assets/Scripts/Models/Star.cs
@@ -3,16 +3,50 @@ using UnityEngine;
 
 namespace Models
 {
-    [RequireComponent(typeof(GravitySource))]
-    public class Star : MonoBehaviour
+    public class Star : GravitySource
     {
         public GravitySystem.GravitySystem GravitySystem;
         public float MinSpawnRate;
         public float MaxSpawnRate;
-        public GravitySource GravitySource;
         public Dot DotPrefab;
         public IList<Dot> Dots;
 
 
+        float _spawnTicker = 0f;
+
+        void Start()
+        {
+            Dots = new List<Dot>();
+           GravitySystem.AddGravitySource(this);
+        }
+
+        void OnDestroy()
+        {
+            GravitySystem.DestroyGravitySource(this);
+        }
+
+        void Update()
+        {
+            SpawnDotRoutine();
+        }
+
+
+
+        /// <summary>
+        /// Uses the spawnRate to create a new Dot when it's time
+        /// </summary>
+        void SpawnDotRoutine()
+        {
+            //When the ticker gets to 0, Spawn the Dot and restart it 
+            if (_spawnTicker <= 0)
+            {
+                _spawnTicker  = Random.Range(MinSpawnRate, MaxSpawnRate);
+                var d = Instantiate(DotPrefab, transform);
+                d.ParentStar = this;
+                Dots.Add(d);
+            }
+
+            _spawnTicker -= Time.deltaTime;
+        }
     }
 }
